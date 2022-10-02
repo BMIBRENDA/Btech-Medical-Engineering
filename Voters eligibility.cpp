7105499777b2cdb4bc0cffbeb45e3c6af102e141
BMIBRENDA <113904082+BMIBRENDA@users.noreply.github.com>
2022-10-02 05:15:49 +0300

    Add files via upload

---
+++ Voters eligibility.cpp
@@ -0,0 +1,19 @@
+HB107/G/8417/20 BRENDA CHERONO KOSGEI
+
+//program to check voter eligibility
+#include<iostream>
+Using namespace std;
+int main()
+{
+Int age;
+Cout<<”Enter your age”<<endl;
+Cin >>age;
+If (age>=18)
+{
+Cout<<”you are eligible to vote”<<endl;
+}
+Else
+{
+Cout<<”not eligible to vote”;
+Return 0;
+}
